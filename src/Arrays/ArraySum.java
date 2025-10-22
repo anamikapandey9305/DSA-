a047e3302f699773d7a39a0ec27b3bc2ed03081a
Anamika Pandey <anamikapndey9305@gmail.com>
2025-10-22 23:35:06 +0530

    Added arrays folders with 4 java files

---
+++ src/Arrays/ArraySum.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] arr = {-6,8 ,14,-2,23,47,-7};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+
+
+        }
+        System.out.println(sum);
+
+    }
+}

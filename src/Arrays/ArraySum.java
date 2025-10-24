e50b71223df0f8a2871271d17248fad61e87e061
Anamika Pandey <anamikapndey9305@gmail.com>
2025-10-25 01:47:05 +0530

    Added ArraySum

--- src/Arrays/ArraySum.java
+++ src/Arrays/ArraySum.java
@@ -1,3 +1,4 @@
+// To find the sum of the  all element in an array
 package Arrays;
 
 public class ArraySum {
@@ -5,8 +6,8 @@ public class ArraySum {
         int[] arr = {-6,8 ,14,-2,23,47,-7};
         //System.out.println(arr[7]); index 7 out of bound for length 7
         int sum=0;
-        for(int i=0;i<arr.length;i++){
-            sum+=arr[i];
+        for(int i=0;i<arr.length;i++){ // loop goes through every element in an array
+            sum+=arr[i]; // adding each element to sum
 
 
         }

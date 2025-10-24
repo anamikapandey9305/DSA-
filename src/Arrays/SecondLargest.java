07e78b8ac609cbe5a5b4c628b7c06b59cfc91aab
Anamika Pandey <anamikapndey9305@gmail.com>
2025-10-25 01:42:52 +0530

    second largest
    Added comments to clarify the purpose of variables and loops.

--- src/Arrays/SecondLargest.java
+++ src/Arrays/SecondLargest.java
@@ -3,8 +3,9 @@ package Arrays;
 public class SecondLargest {
     public static void main(String[] args) {
         int[] arr = {4,10,6,3,8,8};
-        int max = Integer.MIN_VALUE;
-        int smax = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE; // stores the largest number
+        int smax = Integer.MIN_VALUE; // stores the second largest number
+        // INTEGER.MIN_VALUE is the smallest possible integer in java
 
         // Calculate max
         for(int i=0;i<arr.length;i++){
@@ -14,10 +15,17 @@ public class SecondLargest {
         // calculate second largest
         for(int i=0;i<arr.length;i++){
             if(arr[i]>smax && arr[i]!=max) smax = arr[i];
+            // arr[i] > smax → current element is bigger than current second largest
+            
+            //arr[i] != max → current element cannot be the largest
 
         }
         System.out.println(max);
         System.out.println(smax);
+
+       // First loop: Find the largest number.
+
+//Second loop: Find the largest number smaller than max.
 
 
     }

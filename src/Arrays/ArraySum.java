eeeb3927faa534c29f1816ed87e6f0189a0e683e
Anamika Pandey <anamikapndey9305@gmail.com>
2025-10-25 01:18:20 +0530

    AddedSecondLargest

--- src/Arrays/ArraySum.java
+++ src/Arrays/ArraySum.java
@@ -3,6 +3,7 @@ package Arrays;
 public class ArraySum {
     public static void main(String[] args) {
         int[] arr = {-6,8 ,14,-2,23,47,-7};
+        //System.out.println(arr[7]); index 7 out of bound for length 7
         int sum=0;
         for(int i=0;i<arr.length;i++){
             sum+=arr[i];

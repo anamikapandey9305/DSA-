0403afac31905ea8a961882f21ae23b07bb2f0e4
Anamika Pandey <anamikapndey9305@gmail.com>
2025-10-26 04:46:36 +0530

    Add Reverse Array

--- src/Arrays/ReverseArray.java
+++ src/Arrays/ReverseArray.java
@@ -4,8 +4,13 @@ public class ReverseArray {
     public static void main(String[] args) {
         int[] arr = {3,19,56,9,83,18,24,85,14};
         int n = arr.length;
-        int i = 0, j= n-1;
-        while(i<j){
+        int i = 0, j= n-1; // i starts from beginning (0), j starts from end (n-1 = 8)
+
+        while(i<j){ //Swaps the elements at positions i and j.
+
+            // (i++)Moves i one step forward.
+
+                    // (j--)Moves j one step backward.
             int temp = arr[i];
             arr[i]= arr[j];
             arr[j]= temp;
@@ -18,3 +23,7 @@ public class ReverseArray {
 
     }
 }
+
+//You used two pointers (i and j) — one from start, one from end.
+//You swapped elements until they met in the middle.
+//Finally, you printed the reversed array.
